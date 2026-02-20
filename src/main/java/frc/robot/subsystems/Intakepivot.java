7dab45369b3601158b87884f4faf76eef0e0b4c4
claufenberg6-collab <claufenberg6@gmail.com>
2026-02-19 19:28:43 -0600

    changed pivot speed

--- src/main/java/frc/robot/subsystems/Intakepivot.java
+++ src/main/java/frc/robot/subsystems/Intakepivot.java
@@ -17,7 +17,7 @@ import frc.robot.Constants.MechanismConstants;
 
 
 public class Intakepivot extends SubsystemBase {
-  public double IntakepivotSpeed = 1;
+  public double IntakepivotSpeed = 0.5;
   private SparkMax IntakepivotSparkMax = new SparkMax(MechanismConstants.kintakepivotPort, MotorType.kBrushless);
   
    public Intakepivot() {

9f77854ce05682f5ed4f061fcd16d7200ff83340
claufenberg6-collab <claufenberg6@gmail.com>
2026-03-01 13:42:07 -0600

    added shooter clear command maped to x

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,14 +16,10 @@ import frc.robot.commands.*;
 
 
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
+
+
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+ 
   private final DriveSubsystem m_robotDrive = new DriveSubsystem();
    private final Index m_index = new Index();
   private final Bshooterwheel m_bshooterHS = new Bshooterwheel();
@@ -43,15 +39,14 @@ public class RobotContainer {
       private final CommandXboxController m_CoDriverController =
       new CommandXboxController(OIConstants.kCoDriverControllerPort);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+
   public RobotContainer() {
-    // Configure the trigger bindings
+   
     configureBindings();
 
-    // Configure default commands
+  
     m_robotDrive.setDefaultCommand(
-        // The left stick controls translation of the robot.
-        // Turning is controlled by the X axis of the right stick.
+      
         new RunCommand(
             () ->
                 m_robotDrive.drive(
@@ -73,17 +68,9 @@ public class RobotContainer {
     
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
+ 
   private void configureBindings() {
-    // Left Stick Button -> Set swerve to X
+   
     m_driverController.leftStick().whileTrue(m_robotDrive.setXCommand());
     m_driverController.start().onTrue(m_robotDrive.zeroHeadingCommand());
 
@@ -93,34 +80,22 @@ public class RobotContainer {
     m_driverController.leftTrigger().toggleOnTrue(new shooterlong(m_bshooterHS));
     m_driverController.rightTrigger().toggleOnTrue(new flywheelsidewall(m_Flywheel));
      m_driverController.leftTrigger().toggleOnTrue(new flywheeltower(m_Flywheel));
+      m_driverController.x().whileTrue(new shooterclear(m_Flywheel));
 
 
 
     m_driverController.leftStick().whileTrue(m_robotDrive.setXCommand());
     m_driverController.start().onTrue(m_robotDrive.zeroHeadingCommand());
 
+    m_CoDriverController.x().whileTrue(new shooterclear(m_Flywheel));
     m_CoDriverController.leftBumper().toggleOnTrue(new IntakeN(m_intake));
     m_CoDriverController.rightBumper().whileTrue(new IntakepivotN(m_Intakepivot));
     m_CoDriverController.rightTrigger().whileTrue(new pivotdown(m_Intakepivot));
     m_CoDriverController.y().whileTrue(new endgameT(m_endgame));
     m_CoDriverController.a().whileTrue(new endgameout(m_endgame));
-       m_CoDriverController.povUp().whileTrue(new pivotpos1(m_Intakepivot));
-       m_CoDriverController.povDown().whileTrue(new pivotpos2(m_Intakepivot));
-       m_CoDriverController.povRight().whileTrue(new pivotpos3(m_Intakepivot));
-    // Right Trigger -> Run fuel intake in reverse
-   
-
-    // Left Trigger -> Run fuel intake in reverse
-   
-
-    // Y Button -> Run intake and run the shooter flywheel and feeder
-    
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+    m_CoDriverController.povUp().whileTrue(new pivotpos1(m_Intakepivot));
+    m_CoDriverController.povDown().whileTrue(new pivotpos2(m_Intakepivot));
+    m_CoDriverController.povRight().whileTrue(new pivotpos3(m_Intakepivot));
   
   }
 }

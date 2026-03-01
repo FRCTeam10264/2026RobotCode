4ce448f8a23a283f907848f4ae4f2aa530271a8a
claufenberg6-collab <claufenberg6@gmail.com>
2026-03-01 11:08:41 -0600

    got rid of old vender deps that are not used and intalled pathplaner lib and might have fixed pivot positions

--- src/main/java/frc/robot/subsystems/Intakepivot.java
+++ src/main/java/frc/robot/subsystems/Intakepivot.java
@@ -35,15 +35,15 @@ public class Intakepivot extends SubsystemBase {
     IntakepivotSparkMax.stopMotor();
   }
 
-public void intakepos1(double intakepivotSpeed2){
+public void intakepos1(double intakepivotSpeed){
   intakepivioController.setSetpoint(5, ControlType.kPosition);
 
   }
-  public void intakepos2(double intakepivotSpeed2){
+  public void intakepos2(double intakepivotSpeed){
   intakepivioController.setSetpoint(10, ControlType.kPosition);
 
   }
-  public void intakepos3(double intakepivotSpeed2){
+  public void intakepos3(double intakepivotSpeed){
   intakepivioController.setSetpoint(10, ControlType.kPosition);
 
   }

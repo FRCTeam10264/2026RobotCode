3c6d532b92e13c6e220ffd354d21c0cbb249f0ff
claufenberg6-collab <claufenberg6@gmail.com>
2026-03-03 18:06:09 -0600

    no one will know

--- src/main/java/frc/robot/subsystems/Intakepivot.java
+++ src/main/java/frc/robot/subsystems/Intakepivot.java
@@ -35,7 +35,7 @@ public class Intakepivot extends SubsystemBase {
     IntakepivotSparkMax.stopMotor();
   }
 
-public void intakepos1(double intakepivotSpeed){
+public void intakepos1(double Speed){
   intakepivioController.setSetpoint(0, ControlType.kPosition);
 
   }
